321a982adcae193939b4468962cdc45e514b94d2
unknown <rostykvoznyak@gmail.com>
2020-10-27 23:27:10 +0200

    Lesson1Homework3

---
+++ Lesson1Homework3/Lesson1Homework3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson1Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nmy line 1");// означає що після \n слова будуть з іншого рядка
+            Console.WriteLine("\tmy line 2");// означає що після \t буде пропуск у вигляді одного нажаття Tab
+            Console.WriteLine("\amy line 3");//зєднує всі слова разом
+
+
+        }
+    }
+}
